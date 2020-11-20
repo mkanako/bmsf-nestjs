0beea2c43f52023937914f64d339cf932ca04097
kanako <4190346+mkanako@users.noreply.github.com>
2020-11-20 17:15:36 +0800

    up

--- libs/entities/src/admin/attachment.entity.ts
+++ libs/entities/src/admin/attachment.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Exclude } from 'class-transformer'
 import { CreateTimestampColumn, UpdateTimestampColumn } from '@common/decorators/typeorm-timestamp-column'
 
 @Entity('admin_attachments')
@@ -15,15 +16,18 @@ export class AdminAttachment {
   @Column({ length: 20 })
   type: string
 
+  @Exclude()
   @Column({
     default: 0,
     unsigned: true,
   })
   uid: number
 
+  @Exclude()
   @CreateTimestampColumn()
   createdAt: Date
 
+  @Exclude()
   @UpdateTimestampColumn()
   updatedAt: Date
 }
